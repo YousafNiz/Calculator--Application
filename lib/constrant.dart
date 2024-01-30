b5f376dd337ae5659f9757083fd22cbc5b327d87
YousafNiz <149098180+YousafNiz@users.noreply.github.com>
2024-01-30 01:39:23 -0800

    initial done
    initial done

---
+++ lib/constrant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color white = Colors.white;
+
+const Color blueColor = Colors.blue;
+
+const headingTextStyle = TextStyle(fontSize: 20, color: white, fontWeight: FontWeight.w500);
